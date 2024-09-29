933d13cb7aa0824b610081f2a134daa3880c9c73
JaberDarawsheh <jaberdarawsheh@gmail.com>
2024-09-29 15:50:06 +0300

    ITG-1 Add a Form task Done

--- cartridges/trainingcartridge/cartridge/controllers/Home.js
+++ cartridges/trainingcartridge/cartridge/controllers/Home.js
@@ -4,8 +4,20 @@ var server = require('server');
 var cache = require('*/cartridge/scripts/middleware/cache');
 var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
 
-server.get('Page1',consentTracking.consent, cache.applyDefaultCache, function (req, res, next) {
-    res.render('home/page1');
+server.get(
+    'Page1',
+    consentTracking.consent,
+    cache.applyDefaultCache,
+    function (req, res, next) {
+        res.render('home/page1');
+        next();
+    }
+);
+
+server.post('AddNumber', function (req, res, next) {
+    var enteredNumber = parseInt(req.form.number, 10);
+    var incrementalNumber = enteredNumber + 1;
+    res.render('home/page2', { resultnumber: incrementalNumber });
     next();
 });
 
