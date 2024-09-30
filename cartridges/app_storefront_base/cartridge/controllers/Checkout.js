f43b29bca991d9a0c9d0f911f5ab5fc4893f6b06
JaberDarawsheh <jaberdarawsheh@gmail.com>
2024-09-30 16:20:20 +0300

    ITG-3 Modification of order custom attribute task

--- cartridges/app_storefront_base/cartridge/controllers/Checkout.js
+++ cartridges/app_storefront_base/cartridge/controllers/Checkout.js
@@ -54,9 +54,6 @@ server.get(
             res.redirect(URLUtils.url('Cart-Show'));
             return next();
         }
-        // here i will the product quantities inside the cart 
-        var lessQuantitiesItems = validateCartItems(currentBasket);
-        
         var requestStage = req.querystring.stage;
         var currentStage = requestStage || 'customer';
         var billingAddress = currentBasket.billingAddress;
